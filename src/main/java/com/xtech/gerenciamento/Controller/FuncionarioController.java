7ae36cc061ac6097283dce07fa30f507495c00e8
Luiz Otávio <luiz.fontes0@outlook.com>
2026-04-17 16:24:09 -0300

    inserir alternativo

--- src/main/java/com/xtech/gerenciamento/Controller/FuncionarioController.java
+++ src/main/java/com/xtech/gerenciamento/Controller/FuncionarioController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,6 +39,13 @@ public class FuncionarioController {
         FuncionarioBEAN funcionario = servico.lerID(id);      
         model.addAttribute("funcionario", funcionario);
         return "perfil";
+    }
+    
+    @GetMapping("/adicionar-novo")
+    public String adicionarNovo(Model model) {
+        FuncionarioBEAN funcionario = new FuncionarioBEAN();
+        model.addAttribute("funcionario", funcionario);
+        return "adicionar-novo";
     }
     @PostMapping("/salvar")
     public String salvarDados(@ModelAttribute FuncionarioBEAN funcionario){
